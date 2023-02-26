e4f2713fc2842255c17995498a9e9147858dc719
Luz Davico <mldavico@gmail.com>
2023-02-26 16:59:37 -0300

    Adding logic to render the cards based on a JS object

---
+++ js/future-events.js
@@ -0,0 +1,26 @@
+let eventsContainer = document.querySelector(".events-container");
+let htmlEvents = "";
+for(let event of data.events){
+    let currentDate = new Date(data.currentDate);
+    let eventDate = new Date(event.date);
+
+    if (eventDate > currentDate) {
+        eventsContainer.innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="${event.image}" class="card-img-top" alt="${event.description}">
+        <div class="card-body">
+            <h5 class="card-title">${event.name}</h5>
+            <p><small class="text-muted">${event.date}<br><span class="event-category">${event.category}</span></small></p>
+            <p class="card-text">
+            ${event.description}
+            </p>
+            <p><strong>Location:</strong> ${event.place}</p>
+            <p><strong>Capacity:</strong> ${event.capacity}</p>
+            <p><strong>Assistance (estimated):</strong> ${event.estimate}</p>
+            <p>Price: ${event.price}</p>
+            <a href="#" class="btn btn-primary">Read More</a>
+        </div>
+        </div>`;
+    } 
+}
+
+
